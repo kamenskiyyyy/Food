fbf6496f589679491e0516084a89066ffd163adb
Kamenev Nikolay <79969997909@mail.ru>
2021-01-19 19:28:00 +0300

    JSON

--- js/script.js
+++ js/script.js
@@ -233,20 +233,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            // const object = {};
-            // formData.forEach(function(value, key) {
-            //     object[key] = value;
-            // });
-
-            // const json = JSON.stringify(object);
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
 
             fetch('server.php', {
                 method: 'POST',
-                // headers: {
-                //     'Content-type': 'application/json'
-                // },
-                body: formData
-            }).then(data => {
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
                 
